e6190a1888a715c1c81046684abcfd3fb23704f7
fanchen <fanchen@danwings.cn>
2022-10-11 10:07:14 +0800

    初始新建项目

---
+++ src/main/java/cn/dawnings/bookkeeping/BookkeepingBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.dawnings.bookkeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkeepingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookkeepingBackendApplication.class, args);
+    }
+
+}
